16240cf09b2fc5478ae35b07367b8b32b884f193
Immolazione <federico.deered@gmail.com>
2022-02-10 03:18:20 +0100

    change font

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script>
-  import Header from './components/Header.vue'
-  import Main from './components/Main.vue'
-  import Footer from './components/Footer.vue'
+  import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
+  import Footer from './components/Footer.vue';
 
   export default {
     name: 'App',
@@ -22,10 +22,12 @@
 </script>
 
 <style>
-  * {
+@import'_font.scss';
+* {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-style: '$segoe';
   }
   .container {
     width: 1200px;
